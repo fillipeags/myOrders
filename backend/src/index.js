d5509767af5c009d797436ad5450e815e5a6425d
Fillipe Augusto <38387593+fillipeags@users.noreply.github.com>
2021-01-07 09:43:31 -0300

    create routes and start db

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,17 @@
 /* eslint-disable no-console */
 const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+mongoose.connect('mongodb://localhost:27017/ezorders', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 
 const app = express();
+app.use(express.json());
+app.use(routes);
 
 app.listen(3001, () => console.log('🔥 Server started runnning at http://localhost:3001'));
