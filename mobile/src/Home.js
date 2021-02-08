0d00e8359d5057a2dd6c1f17f2a8e9b9603d3843
Fillipe Augusto <fillipe484@gmail.com>
2021-02-07 21:21:53 -0300

    newOrder button component

--- mobile/src/Home.js
+++ mobile/src/Home.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { Text } from "react-native";
 
-import { Container, Logo } from "./styles/Index";
+import { Container, Logo, NewOrderContainer } from "./styles/Index";
+
+import Orders from "./components/Orders";
+import Button from "./components/Button";
+
 import logo from "./images/logo.png";
 
 export default function Home() {
   return (
     <Container>
       <Logo source={logo} resizeMode="contain" />
+
+      <Orders />
+      <NewOrderContainer>
+        <Button>
+          <Button.Label>Novo Pedido</Button.Label>
+        </Button>
+      </NewOrderContainer>
     </Container>
   );
 }
--- mobile/src/styles/Index.js
+++ mobile/src/styles/Index.js
@@ -8,3 +8,14 @@ export const Container = styled.SafeAreaView`
 export const Logo = styled.Image`
   margin-top: 32px;
 `;
+
+export const NewOrderContainer = styled.View`
+  background: white;
+  height: 90px;
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  box-shadow: 0px 0px 13px rgba(0, 0, 0, 0.25);
+  padding: 0 24px;
+  justify-content: center;
+`;
